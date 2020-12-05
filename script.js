303c094d97e7a49f261a53908f3132fa22d4644b
EjMat8 <franzelijahmatugas@gmail.com>
2020-12-05 18:38:10 +0800

    overlay

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 "use strict";
+
+const buttonSignup = document.querySelector("signupbutton");
+const overlay = document.querySelector("overlay");
+
+buttonSignup.addEventListener("click", function (e) {
+  e.preventDefault();
+  overlay.classList.remove("hidden");
+});
