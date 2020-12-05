bcfa2550455a35e262649957c4eb8ed397bff360
EjMat8 <franzelijahmatugas@gmail.com>
2020-12-05 20:07:46 +0800

    sup

--- assets/script.js
+++ assets/script.js
@@ -2,13 +2,20 @@
 
 const buttonSignup = document.querySelector(".signupbutton");
 const overlay = document.querySelector(".overlay");
+const modal = document.querySelector(".modal");
 
 buttonSignup.addEventListener("click", function (e) {
   e.preventDefault();
   overlay.classList.remove("hidden");
+  modal.classList.remove("hidden");
 });
 document.addEventListener("keydown", function (e) {
   if (e.key === "Escape") {
     overlay.classList.add("hidden");
+    modal.classList.add("hidden");
   }
+});
+overlay.addEventListener("click", function () {
+  overlay.classList.remove("hidden");
+  modal.classList.remove("hidden");
 });
