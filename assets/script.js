4d4f63027c162daaeb604aa7c90351599f8b43e5
EjMat8 <franzelijahmatugas@gmail.com>
2020-12-05 22:00:36 +0800

    login

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,32 @@
 "use strict";
-//Selected Elements
+//Fake Account
+class Account {
+  constructor(email, password) {
+    this._email = email;
+    this._password = password;
+  }
+  get password() {
+    return this._password;
+  }
+  get email() {
+    return this._email;
+  }
+}
+const account1 = new Account("sample@gmail.com", "password");
+const account2 = new Account("ej@gmail.com", "password123");
+const accounts = [account1, account2];
+
+// const accntLog = accounts.find((accnt) => accnt.email === "sample@gmail.com");
+// console.log(accntLog);
+
+//Selected Elements,
 const buttonSignup = document.querySelector(".signupbutton");
 const overlay = document.querySelector(".overlay");
 const modal = document.querySelector(".modal");
 const navHead = document.querySelector(".nav_head");
+const loginEmail = document.querySelector(".loginemail");
+const loginPass = document.querySelector(".loginpass");
+const loginButton = document.querySelector("#login");
 
 //Functions
 const removeHidden = function () {
@@ -43,3 +66,12 @@ document.addEventListener("keydown", function (e) {
 
 navHead.addEventListener("mouseover", hoverNav.bind(0.5));
 navHead.addEventListener("mouseout", hoverNav.bind(1));
+
+loginButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  const accntLog = accounts.find((accnt) => accnt.email === loginEmail.value);
+
+  if (accntLog && accntLog.password === loginPass.value) {
+    console.log("login");
+  }
+});
