79e2a9b898d178900774d304dcf83e9aa7f22592
EjMat8 <franzelijahmatugas@gmail.com>
2020-12-05 18:48:17 +0800

    moreChanges

--- script.js
+++ script.js
@@ -7,3 +7,8 @@ buttonSignup.addEventListener("click", function (e) {
   e.preventDefault();
   overlay.classList.remove("hidden");
 });
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    overlay.classList.add("hidden");
+  }
+});
