9a4a294ae47b0327fe6cc3525ae8b34088f4167f
keylperez <perez.williamcaleb@gmail.com>
2020-12-10 13:09:20 +0800

    finished settings

--- assets/settings.js
+++ assets/settings.js
@@ -1,22 +1,6 @@
-$(document).ready(function() {
-	
-    var readURL = function(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-
-            reader.onload = function (e) {
-                $('.profile-pic').attr('src', e.target.result);
-            }
-    
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
-   
-    $(".file-upload").on('change', function(){
-        readURL(this);
-    });
-    
-    $(".upload-button").on('click', function() {
-       $(".file-upload").click();
-    });
-});
+function uploadAvatar(event) {
+    const image = document.getElementById('output');
+    image.src = URL.createObjectURL(event.target.files[0]);
+    // const icon = document.getElementById('icon');
+    // icon.style.display = "none";
+  }
